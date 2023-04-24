228f775f84608b41ca28a7b34a89b6728de38bb9
ArtemBehter <www.behchik@gmail.com>
2023-04-24 21:37:09 +0200

    Updates

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import '../../../src/App.scss';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+import Footer from '../Footer';
+
+function Home () {
+    return (
+        <>
+        <HeroSection />
+        <Cards/>
+        <Footer/>
+        </>
+    )
+}
+
+export default Home;
